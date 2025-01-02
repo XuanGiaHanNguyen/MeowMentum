c2cf3f00f3fa3482b5539b1a77442e29bb69eee2
XuanGiaHanNguyen <han316@usf.edu>
2025-01-02 17:07:34 -0500

    main page created

--- frontend/src/assets/icon.jsx
+++ frontend/src/assets/icon.jsx
@@ -9,3 +9,52 @@ export const CardIcon = (
   <path fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clip-rule="evenodd"/>
 </svg>
 );
+
+export const OverviewIcon = (
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M4.857 3A1.857 1.857 0 0 0 3 4.857v4.286C3 10.169 3.831 11 4.857 11h4.286A1.857 1.857 0 0 0 11 9.143V4.857A1.857 1.857 0 0 0 9.143 3H4.857Zm10 0A1.857 1.857 0 0 0 13 4.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 9.143V4.857A1.857 1.857 0 0 0 19.143 3h-4.286Zm-10 10A1.857 1.857 0 0 0 3 14.857v4.286C3 20.169 3.831 21 4.857 21h4.286A1.857 1.857 0 0 0 11 19.143v-4.286A1.857 1.857 0 0 0 9.143 13H4.857Zm10 0A1.857 1.857 0 0 0 13 14.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 19.143v-4.286A1.857 1.857 0 0 0 19.143 13h-4.286Z" clip-rule="evenodd"/>
+</svg>
+  );
+
+export const ScheduleIcon = (
+    <svg class="w-6 h-6 text-gray-800 dark:text-white" style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z" clip-rule="evenodd"/>
+</svg>
+    );
+
+export const TodoIcon = (
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+    <path fill-rule="evenodd" d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-3 8a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-2-1a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H9Zm2 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-2-1a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H9Z" clip-rule="evenodd"/>
+  </svg>
+      );
+
+export const HabitIcon = (
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" style={{ color: '#747474' }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+</svg>
+);
+
+export const SettingsIcon =(
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
+</svg>
+);
+
+export const LogOutIcon =(
+  <svg class="w-6 h-6 dark:text-white " style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M5 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0Zm-2 9a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-1Zm13-6a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4Z" clip-rule="evenodd"/>
+</svg>
+);
+
+export const SearchIcon =(
+  <svg class="w-5 h-5 dark:text-white absolute top-2.5 left-3" style={{ color: '#747474' }} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" strokeWidth="2"/>
+  </svg>
+);
+
+export const userIcon = (
+  <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+</svg>
+);
+
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -3,9 +3,9 @@ import "../../src/index.css";
 import { useNavigate } from "react-router-dom";
 import mainbg from "../assets/mainbg.png";
 import logo from "../assets/logo.png";
+import { OverviewIcon, ScheduleIcon, TodoIcon, HabitIcon, SettingsIcon, LogOutIcon, SearchIcon, userIcon  } from "../assets/icon";
 
 const MainPage = () => {
-
   const navigate = useNavigate();
 
   return (
@@ -16,38 +16,73 @@ const MainPage = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        height: "100vh", // Fallback for h-screen issues
-        width: "100vw",  // Ensure full width
       }}
     >
-      <div className=" h-screen w-screen flex">
-        <div className="flex flex-col px-14 text-2xl"style={{backgroundColor: '#f0f0f0'}}>
-            <div>
-                <img src={logo} alt="logo" width="200" className="mt-8 mb-10"/>
-            </div>
-            <button>
-                Overview
+      <div className="h-screen w-screen flex">
+        {/* Sidebar */}
+        <div
+          className="flex flex-col px-10 text-xl"
+          style={{ backgroundColor: "#f0f0f0" }}
+        >
+          <div>
+            <img src={logo} alt="logo" width="170" className="mt-8 mb-10" />
+          </div>
+          <div className="flex flex-col gap-6 h-full">
+            <button className="text-left flex flex-row font-semibold text-gray-500">
+              &nbsp;{OverviewIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Overview
             </button>
-            <button>
-                Schedule
+            <button className="text-left flex flex-row font-semibold text-gray-500">
+              &nbsp;{ScheduleIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Schedule
             </button>
-            <button>
-                To-do List
+            <button className="text-left flex flex-row font-semibold text-gray-500">
+              &nbsp;{TodoIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To-do List
             </button>
-            <button>
-                Habit Rabbit
+            <button className="text-left flex flex-row font-semibold text-gray-500">
+              &nbsp;{HabitIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Habit Rabbit
             </button>
-            <button>
-                Log Out
+            <button className="text-left flex flex-row font-semibold text-gray-500">
+              &nbsp;{SettingsIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Settings
             </button>
+            <button className="text-left flex flex-row mt-auto mb-10 font-semibold text-gray-500">
+              &nbsp;{LogOutIcon} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log Out
+            </button>
+          </div>
         </div>
-        <div className="flex-1">
-            <div className="flex py-8 px-5 justify-between">
-                <h2 className="">Dashboard</h2>
-                <input type="text" placeholder="Search" className="" />
+
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col">
+          {/* Header */}
+          <div className="py-5 px-5">
+            <div className="flex justify-between">
+              <h2 className="font-bold text-3xl" style={{ color: "#666666" }}>Dashboard</h2>
+              <div className="relative ml-80"> 
+                <input
+                type="text"
+                placeholder="Search"
+                className="rounded-2xl h-10 w-72 bg-gray-100 pl-10 pr-10 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                aria-label="Search"
+                />
+                <div>{SearchIcon}</div>
+              </div>
+              <div>
+                <div className="rounded-full p-2 mb-2 mr-5" style={{backgroundColor: '#c7e6ed'}}>{userIcon}</div>
+              </div>
             </div>
-            <div className="flex-1 h-screen" style={{backgroundColor: '#f7f7f7'}} >
-                <p>Hello</p>
+          </div>
+
+          {/* Content Section */}
+          <div
+            className="flex-1 p-5 bg-gray-100 flex-start"
+          >
+              <div className="border-2 h-full grid grid-rows-3 grid-cols-3">
+                <div className="border-2" style={{backgroundColor: '#c7e6ed'}}>1</div>
+                <div className="border-2" style={{backgroundColor: '#c7e6ed'}}>2</div>
+                <div className="border-2 row-span-2" style={{backgroundColor: '#c7e6ed'}}>3</div>
+                <div className="border-2" style={{backgroundColor: '#c7e6ed'}}>4</div>
+                <div className="border-2" style={{backgroundColor: '#c7e6ed'}}>5</div>
+                <div className="border-2 col-span-2" style={{backgroundColor: '#c7e6ed'}}>6</div>
+                <div className="border-2" style={{backgroundColor: '#c7e6ed'}}>7</div>
+              </div>
             </div>
         </div>
       </div>
